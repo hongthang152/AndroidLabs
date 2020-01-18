20f0f1020e731dd04ce55bdd7b5a46539d6fdc8d
Thang Nguyen <nguyenhongthang1998@gmail.com>
2020-01-17 22:23:34 -0500

    Added 3 layouts for Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -24,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getText(isChecked ? R.string.message_on : R.string.message_off), Snackbar.LENGTH_LONG);
                 snackBar.setAction(getResources().getText(R.string.undo), click -> buttonView.setChecked(!isChecked));
                 snackBar.show();
-                // do something, the isChecked will be
-                // true if the switch is in the On position
             }
         });
     }
